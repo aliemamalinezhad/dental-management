f45ef85c125dd3653ac7bb2b45178829d3011385
ali <aliemamalinezhad@gmail.com>
2022-02-20 18:00:07 +0330

    create env file for production

---
+++ src/dental_api/settings/production.py
@@ -0,0 +1,18 @@
+import dental_api.settings.base as base
+import os
+from pathlib import Path
+from decouple import config
+
+# SECRET_KEY = config('SECRET_KEY')
+# DEBUG = config('DEBUG')
+# # ...
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('NAME'),
+#         'USER': config('USER'),
+#         'PASSWORD': config('PASSWORD'),
+#         'HOST': config('HOST'),
+#         'PORT': '',
+#     }
+# }
